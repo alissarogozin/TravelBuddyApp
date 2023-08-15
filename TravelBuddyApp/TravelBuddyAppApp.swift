23ea1f281bd78fbcb99ce9d6f11134f5a1b7be3c
Alissa Rogozin <alissa.rogozin@gmail.com>
2023-08-15 11:24:59 -0400

    Initial Commit

---
+++ TravelBuddyApp/TravelBuddyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelBuddyAppApp.swift
+//  TravelBuddyApp
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelBuddyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
